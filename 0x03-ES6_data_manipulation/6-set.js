8fe56f7abcd1bfa262e4e9bcb9582044561f3860
leehomeecky <meecky4chist@gmail.com>
2024-01-10 20:39:01 +0100

    solution

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Michael Olawunmi <https://github.com/leehomeecky>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
